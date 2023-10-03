b4ced2905b7d86f4145bcc11490f17e26d840d61
Chiaki Obara <obara@ham.works>
2023-10-03 17:07:12 +0900

    add: html template

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,12 @@
 import './globals.css'
+import 'sanitize.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import React from 'react'
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleDoubleUp } from '@fortawesome/free-solid-svg-icons'
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -15,8 +19,248 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="ja">
+      <body>
+        <div id="container">
+          <header>
+            <h1 id="logo">
+              <a href="index.html">
+                <img src="images/logo_home.png" alt="SAMPLE PET SHOP" />
+              </a>
+            </h1>
+            <aside>
+              <div className="slide1">slide1</div>
+              <div className="slide2">slide2</div>
+              <div className="slide3">slide3</div>
+            </aside>
+            <nav className="category-menu">
+              <ul>
+                <li>
+                  <a href="list.html">
+                    <span className="line1">
+                      <span>子犬</span>を探す
+                    </span>
+                    <span className="line2">Find puppy dogs.</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="list.html">
+                    <span className="line1">
+                      <span>子猫</span>を探す
+                    </span>
+                    <span className="line2">Find puppy cats.</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="list.html">
+                    <span className="line1">
+                      <span>小動物</span>を探す
+                    </span>
+                    <span className="line2">ハムスターや小鳥など</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="list.html">
+                    <span className="line1">
+                      <span>お魚</span>を探す
+                    </span>
+                    <span className="line2">熱帯魚など</span>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </header>
+          <div id="contents">{children}</div>
+          <div className="pagetop">
+            <a href="#">
+              <FontAwesomeIcon icon={faAngleDoubleUp} size="xs" />
+            </a>
+          </div>
+          <div id="footer-contents">
+            <div className="left">
+              <h4 id="footer-logo">
+                <img src="images/logo.png" alt="SAMPLE PET SHOP" />
+              </h4>
+              <p>
+                〒000-0000 東京都千代田区XXXXXX1丁目1号
+                <br />
+                電話：000-0000-0000
+                <br />
+                営業時間：10:00〜20:00まで
+                <br />
+                定休日：毎週水曜日
+              </p>
+
+              <table className="week">
+                <caption>トリミングサロンの受付（△…午前中のみ）</caption>
+                <thead>
+                  <tr>
+                    <th>日</th>
+                    <th>月</th>
+                    <th>火</th>
+                    <th>水</th>
+                    <th>木</th>
+                    <th>金</th>
+                    <th>土</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>△</td>
+                    <td>○</td>
+                    <td>×</td>
+                    <td>○</td>
+                    <td>○</td>
+                    <td>○</td>
+                    <td>△</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+
+            <div className="right">
+              <h4>アクセス</h4>
+              <p>
+                <a href="https://template-party.com/file/pickup_googlemap.html">
+                  GoogleMapの地図を変更する方法はマニュアルをご覧下さい。
+                </a>
+              </p>
+
+              <div className="iframe-box">
+                <iframe
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3241.0448418561778!2d139.74267721573196!3d35.675897837870934!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188b89b2e1c8b1%3A0x59a123e3d5ac5ab6!2z44CSMTAwLTAwMTQg5p2x5Lqs6YO95Y2D5Luj55Sw5Yy65rC455Sw55S677yR5LiB55uu77yX4oiS77yR!5e0!3m2!1sja!2sjp!4v1545036128899"
+                  style={{ border: 0 }}
+                  allowFullScreen
+                ></iframe>
+              </div>
+            </div>
+          </div>
+          <div id="footermenu">
+            <ul>
+              <li className="title">メニュー見出し</li>
+              <li>
+                <a href="#">サンプルメニューサンプルメニュー</a>
+              </li>
+              <li>
+                <a href="#">サンプルメニュー</a>
+              </li>
+              <li>
+                <a href="#">サンプルメニュー</a>
+              </li>
+              <li>
+                <a href="#">サンプルメニュー</a>
+              </li>
+            </ul>
+            <ul>
+              <li className="title">メニュー見出し</li>
+              <li>
+                <a href="#">サンプルメニューサンプルメニュー</a>
+              </li>
+              <li>
+                <a href="#">サンプルメニュー</a>
+              </li>
+              <li>
+                <a href="#">サンプルメニュー</a>
+              </li>
+              <li>
+                <a href="#">サンプルメニュー</a>
+              </li>
+            </ul>
+            <ul>
+              <li className="title">メニュー見出し</li>
+              <li>
+                <a href="#">サンプルメニューサンプルメニュー</a>
+              </li>
+              <li>
+                <a href="#">サンプルメニュー</a>
+              </li>
+              <li>
+                <a href="#">サンプルメニュー</a>
+              </li>
+            </ul>
+            <ul>
+              <li className="title">メニュー見出し</li>
+              <li>
+                <a href="#">
+                  サンプルメニューサンプルメニューサンプルメニューサンプルメニュー
+                </a>
+              </li>
+              <li>
+                <a href="#">サンプルメニュー</a>
+              </li>
+            </ul>
+          </div>
+          <footer>
+            <small>
+              Copyright&copy; <a href="index.html">SAMPLE PET SHOP</a> All
+              Rights Reserved.
+            </small>
+            <span className="pr">
+              <a href="https://template-party.com/" target="_blank">
+                《Web Design:Template-Party》
+              </a>
+            </span>
+          </footer>
+        </div>
+        <div id="menubar">
+          <nav>
+            <ul>
+              <li>
+                <a href="index.html">ホーム</a>
+              </li>
+              <li>
+                <a href="list.html">子犬を探す</a>
+              </li>
+              <li>
+                <a href="list.html">子猫を探す</a>
+              </li>
+              <li>
+                <a href="list.html">小動物を探す</a>
+              </li>
+              <li>
+                <a href="list.html">お魚を探す</a>
+              </li>
+            </ul>
+          </nav>
+
+          <nav className="category-menu">
+            <ul>
+              <li>
+                <a href="list.html">
+                  <span className="line1">
+                    <span>子犬</span>を探す
+                  </span>
+                  <span className="line2">Find puppy dogs</span>
+                </a>
+              </li>
+              <li>
+                <a href="list.html">
+                  <span className="line1">
+                    <span>子猫</span>を探す
+                  </span>
+                  <span className="line2">Find puppy cats</span>
+                </a>
+              </li>
+              <li>
+                <a href="list.html">
+                  <span className="line1">
+                    <span>小動物</span>を探す
+                  </span>
+                  <span className="line2">ハムスターや小鳥など</span>
+                </a>
+              </li>
+              <li>
+                <a href="list.html">
+                  <span className="line1">
+                    <span>お魚</span>を探す
+                  </span>
+                  <span className="line2">熱帯魚など</span>
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,93 +3,89 @@ import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <>
+      <main>
+        <section>
+          <h2>テンプレートのご利用前に必ずお読み下さい</h2>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
+          <h3>利用規約のご案内</h3>
           <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            このテンプレートは、
+            <a href="https://template-party.com/">Template Party</a>
+            にて無料配布している『ペットショップ・ブリーダー向け無料ホームページテンプレート
+            tp_petshop1』です。必ずダウンロード先のサイトの
+            <a href="https://template-party.com/read.html">利用規約</a>
+            をご一読の上でご利用下さい。
           </p>
-        </a>
-      </div>
-    </main>
+          <p>
+            <strong className="color-check">
+              HP最下部の著作表示『Web
+              Design:Template-Party』は無断で削除しないで下さい。
+            </strong>
+            <br />
+            わざと見えなく加工する事も禁止です。
+          </p>
+          <p>
+            <strong className="color-check">下部の著作を外したい場合は</strong>
+            <br />
+            <a href="https://template-party.com/">Template-Party</a>の
+            <a href="https://template-party.com/member.html">ライセンス契約</a>
+            を行う事でHP下部の著作を外す事ができます。
+          </p>
+
+          <h3>当テンプレートの詳しい使い方は</h3>
+          <p>
+            <a href="about.html">こちらをご覧下さい。</a>
+          </p>
+        </section>
+
+        <section>
+          <h2>お知らせ</h2>
+
+          <dl className="new">
+            <dt>
+              2023/09/03<span>アイコン</span>
+            </dt>
+            <dd>
+              list.htmlの.listのスタイルで、なくてもいいスタイル(grid-row)が数箇所あったので削除。既にご利用中のお客様はそのままでも問題はございません。
+            </dd>
+            <dt>
+              2023/08/23<span>アイコン</span>
+            </dt>
+            <dd>
+              ペットショップ・ブリーダー向け無料ホームページテンプレート
+              tp_petshop1公開。
+            </dd>
+            <dt>
+              20XX/00/00<span className="icon-bg1">アイコン１</span>
+            </dt>
+            <dd>サンプルテキスト。サンプルテキスト。サンプルテキスト。</dd>
+            <dt>
+              20XX/00/00<span className="icon-bg2">アイコン２</span>
+            </dt>
+            <dd>サンプルテキスト。サンプルテキスト。サンプルテキスト。</dd>
+            <dt>20XX/00/00</dt>
+            <dd>サンプルテキスト。サンプルテキスト。サンプルテキスト。</dd>
+            <dt>20XX/00/00</dt>
+            <dd>
+              サンプルテキスト。サンプルテキスト。サンプルテキスト。サンプルテキスト。サンプルテキスト。サンプルテキスト。サンプルテキスト。サンプルテキスト。サンプルテキスト。サンプルテキスト。サンプルテキスト。サンプルテキスト。
+            </dd>
+          </dl>
+        </section>
+
+        <section>
+          <h2 className="dn">フォトギャラリー</h2>
+
+          <div className="thumbnail-slide">
+            <img src="images/1.jpg" alt="" />
+            <img src="images/2.jpg" alt="" />
+            <img src="images/3.jpg" alt="" />
+            <img src="images/1.jpg" alt="" />
+            <img src="images/2.jpg" alt="" />
+            <img src="images/3.jpg" alt="" />
+          </div>
+        </section>
+      </main>
+    </>
   )
 }
