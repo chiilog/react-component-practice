cb11952a9da3eaecd0bfe0ce8a624123caac183d
Chiaki Obara <obara@ham.works>
2023-10-04 13:44:01 +0900

    fix: eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,39 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires,import/no-extraneous-dependencies
+const config = require('eslint-config-next')
+
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  ...config,
+  parser: 'eslint-config-next/parser',
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    ...config.extends,
+    'next',
+    'next/core-web-vitals',
+    'airbnb',
+    'airbnb-typescript',
+    'prettier',
+  ],
+  overrides: [
+    ...config.overrides,
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
     },
-    "extends": [
-        "airbnb",
-        "airbnb-typescript",
-        "prettier",
-    ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "project": "./tsconfig.json"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
+  ],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  plugins: ['react'],
+  rules: {
+    ...config.rules,
+    'jsx-a11y/anchor-is-valid': 'off',
+  },
 }
