0db02ed1626f317a015ced1789188b76fb5edea6
Chiaki Obara <obara@ham.works>
2023-10-10 15:29:40 +0900

    サイズ感の調整、クラスの共通化

--- src/components/atoms/hamburgerButton/hamburgerButton.tsx
+++ src/components/atoms/hamburgerButton/hamburgerButton.tsx
@@ -21,7 +21,7 @@ function Button({
 }) {
   return (
     <button
-      className="w-[46px] h-[46px] flex flex-col justify-between bg-black/50 rounded-full scale-[1.3] py-[17px] px-[13px]"
+      className="w-[46px] h-[46px] flex flex-col justify-between bg-black/50 rounded-full scale-[1.3] py-[15px] px-[13px]"
       onClick={onClick}
       type="button"
     >
@@ -34,20 +34,26 @@ export default function HamburgerButton({
   onClick,
   isOpen,
 }: HamburgerButtonProps) {
+  const commonClassName =
+    'block transition-transform duration-300 border-t-2 border-t-solid border-t-white';
   if (isOpen) {
     return (
       <Button onClick={onClick}>
-        <span className="block w-[20px] transition-transform duration-300 border-t-2 border-solid border-white origin-center rotate-45 translate-x-0 translate-y-[5px]" />
+        <span
+          className={`w-[20px] origin-center rotate-45 translate-x-0 translate-y-[7px] ${commonClassName}`}
+        />
         <span className="hidden" />
-        <span className="block w-[20px] transition-transform duration-300 border-t-2 border-solid border-white origin-center -rotate-45 translate-x-0 translate-y-[-5px]" />
+        <span
+          className={`w-[20px] origin-center -rotate-45 translate-x-0 translate-y-[-7px] ${commonClassName}`}
+        />
       </Button>
     );
   }
   return (
     <Button onClick={onClick}>
-      <span className="block w-full transition-transform duration-300 border-t-2 border-solid border-white" />
-      <span className="block w-full transition-transform duration-300 border-t-2 border-solid border-white" />
-      <span className="block w-full transition-transform duration-300 border-t-2 border-solid border-white" />
+      <span className={`w-full ${commonClassName}`} />
+      <span className={`w-full ${commonClassName}`} />
+      <span className={`w-full ${commonClassName}`} />
     </Button>
   );
 }
