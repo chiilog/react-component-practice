d2fe5e66d8ae4a000ead25e623b15d8e24b8ca7b
Chiaki Obara <obara@ham.works>
2023-10-23 14:11:41 +0900

    fix: Heading add className props

--- src/components/atoms/Heading/Heading.tsx
+++ src/components/atoms/Heading/Heading.tsx
@@ -1,19 +1,36 @@
 import React from 'react';
 
+import clsx from 'clsx';
+
 interface HeadingProps {
+  className?: string;
   level: 'h2' | 'h3';
   children: React.ReactNode;
 }
-export default function Heading({ level = 'h2', children }: HeadingProps) {
+export default function Heading({
+  className,
+  level = 'h2',
+  children,
+}: HeadingProps) {
   if (level === 'h2') {
     return (
-      <h2 className="bg-primary text-white rounded-full py-3.5 px-8 text-lg lg:text-xl mb-8">
+      <h2
+        className={clsx(
+          'bg-primary text-white rounded-full py-3.5 px-8 text-lg lg:text-xl',
+          className,
+        )}
+      >
         {children}
       </h2>
     );
   }
   return (
-    <h3 className="bg-neutral-100 text-neutral-600 rounded-full py-3.5 px-8 text-base lg:text-lg mb-8">
+    <h3
+      className={clsx(
+        'bg-neutral-100 text-neutral-600 rounded-full py-3.5 px-8 text-base lg:text-lg',
+        className,
+      )}
+    >
       {children}
     </h3>
   );
