df4e3e457b9cb0ca3cef196bb54d11456bff936f
Chiaki Obara <obara@ham.works>
2023-10-24 09:46:37 +0900

    TextLinkにisExternalを追加

--- src/components/atoms/TextLink/TextLink.tsx
+++ src/components/atoms/TextLink/TextLink.tsx
@@ -5,14 +5,29 @@ import Link from 'next/link';
 interface TextLinkProps {
   children: React.ReactNode;
   href: string;
+  isExternal?: boolean;
 }
+const className = 'underline hover:text-primary transition-colors duration-300';
 
-export default function TextLink({ children, href }: TextLinkProps) {
+export default function TextLink({
+  children,
+  href,
+  isExternal,
+}: TextLinkProps) {
+  if (isExternal) {
+    return (
+      <a
+        href={href}
+        className={className}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {children}
+      </a>
+    );
+  }
   return (
-    <Link
-      href={href}
-      className="underline hover:text-primary transition-colors duration-300"
-    >
+    <Link href={href} className={className}>
       {children}
     </Link>
   );
